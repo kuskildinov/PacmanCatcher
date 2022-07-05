402dee528cd5109aaf5d6ea211b49ad30dded44e
kuskildinov <kuskildinural@gmail.com>
2022-07-05 23:57:40 +0300

    Pacman
    Clicker when you should kill pacman

---
+++ PackmanCatcher/Assets/Scripts/GhostScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GhostScript : MonoBehaviour
+{
+    bool MousePushed = false;
+
+    public Text scoreText;
+    public GameObject restartPanel;
+
+    private int score;
+
+    private void Start()
+    {
+        scoreText.text = "1";
+    }
+    void Update()
+    {
+        Vector2 cursorPosition = Input.mousePosition;                       //считывает позицию курсора 
+
+        cursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);    //ограничивает позицию курсора полями камеры
+
+        if (MousePushed) transform.position = cursorPosition;
+
+        scoreText.text = score.ToString();
+
+        if(score == 5)
+        {
+            restartPanel.SetActive(true);
+        }
+    }
+
+
+    /// <summary>
+    /// Зажал кнопку мыши
+    /// </summary>
+    public void OnMouseDown()
+    {
+       
+        MousePushed = true;
+        
+    }
+
+    /// <summary>
+    /// Отпустил кнопку мыши
+    /// </summary>
+    public void OnMouseUp()
+    {
+        
+        MousePushed = false;
+    }
+
+    /// <summary>
+    /// Срабатывает триггер при соприкосновении призрака и пакмана
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        score++;
+        other.gameObject.SetActive(false);
+    }
+    /// <summary>
+    /// Перезапуск сцены
+    /// </summary>
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    /// <summary>
+    /// Выход из игры
+    /// </summary>
+    public void exitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ PackmanCatcher/Assets/Scripts/GhostsRandom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// В случайном порядке начинает игру за одно из призраков
+/// </summary>
+public class GhostsRandom : MonoBehaviour
+{
+    int rand;
+
+    public GameObject blueGhost;
+    public GameObject yellowGhost;
+    public GameObject redGhost;
+
+    void Start()
+    {
+        rand = Random.Range(1, 4);
+        Debug.Log(rand);
+        switch(rand)
+        {
+            case 1: blueGhost.SetActive(true);
+                break;
+            case 2:
+               redGhost.SetActive(true);
+                break;
+            case 3:
+               yellowGhost.SetActive(true);
+                break;
+            default:
+                blueGhost.SetActive(true);
+                    break;
+        }
+    }
+
+}
